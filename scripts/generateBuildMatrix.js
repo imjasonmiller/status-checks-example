3c116d6cc4b5042874b0caf9d5937f67b9b38361
Jason Miller <contact@jasonmiller.nl>
2023-06-09 10:29:12 +0200

    feat: add testing matrix

---
+++ scripts/generateBuildMatrix.js
@@ -0,0 +1,7 @@
+const generatBuildMatrix = (filesAdded, filesModified, filesRenamed) => {
+  console.log("filesAdded:", filesAdded, "filesModified:", filesModified, "filesRenamed:", filesRenamed);
+
+  const clients = [];
+
+  return null;
+};
