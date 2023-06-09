4bcfe18b23cf0d3e7a2a567388982d5321e01747
Jason Miller <contact@jasonmiller.nl>
2023-06-09 14:31:46 +0200

    feat/testing matrix (#8)

---
+++ apps/bar/mul.ts
@@ -0,0 +1,3 @@
+const mul = (a: number, b: number): number => {
+  return a * b;
+}
---
+++ ci/generateBuildMatrix.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const path = require('path');
+const fs = require('fs');
+
+const getAffectedClients = (filesAdded, filesModified, filesRenamed) => {
+  const files = new Set([...filesAdded, ...filesModified, ...filesRenamed]);
+  const clients = fs
+    .readdirSync(path.resolve(process.cwd(), 'apps'), { withFileTypes: true })
+    .filter((dirent) => {
+      if (!dirent.isDirectory()) {
+        return;
+      }
+
+      return files.has(path.join('apps', dirent.name));
+    })
+    .map((dirent) => ({ app: dirent.name }));
+
+  return clients;
+};
+
+const generateBuildMatrix = (filesAdded, filesModified, filesRenamed) => {
+  const clients = getAffectedClients(filesAdded, filesModified, filesRenamed);
+
+  return clients.length 
+    ? { include: clients }
+    : null;
+};
+
+module.exports = generateBuildMatrix;
