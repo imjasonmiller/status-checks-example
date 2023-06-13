5012bad5c7f807e53435b6454ac85359d75a6d04
Jason Miller <contact@jasonmiller.nl>
2023-06-14 00:19:53 +0200

    fix: add prettier and example tests

--- apps/foo/add.spec.ts
+++ apps/foo/add.spec.ts
@@ -3,18 +3,18 @@ import { add } from "./add";
 
 describe("can add numbers", () => {
   test("add two positive integers", () => {
-    expect(add(1,1)).toBe(2);
-    expect(add(2,3)).toBe(5);
+    expect(add(1, 1)).toBe(2);
+    expect(add(2, 3)).toBe(5);
   });
 
   test("add two negative integers", () => {
-    expect(add(-1,-1)).toBe(-2);
-    expect(add(-1,-2)).toBe(-3);
+    expect(add(-1, -1)).toBe(-2);
+    expect(add(-1, -2)).toBe(-3);
   });
 
   test("can add zero", () => {
-    expect(add(1,0)).toBe(1);
-    expect(add(0,1)).toBe(1);
-    expect(add(0,0)).toBe(0);
+    expect(add(1, 0)).toBe(1);
+    expect(add(0, 1)).toBe(1);
+    expect(add(0, 0)).toBe(0);
   });
 });
